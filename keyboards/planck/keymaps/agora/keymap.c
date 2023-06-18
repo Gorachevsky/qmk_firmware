30b879c76bc35a594f1a92c18161714e2742597b
Gorachevsky <anton.gorachevskiy@gmail.com>
2023-06-18 18:29:50 +0200

    Changed special comma for pipe in lower layout

--- keyboards/planck/keymaps/agora/keymap.c
+++ keyboards/planck/keymaps/agora/keymap.c
@@ -130,7 +130,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_LOWER] = LAYOUT_planck_grid(
       _______,  KC_7,     KC_8,     KC_9,     KC_0,     KC_HASH,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN, KC_RPRN, KC_DEL,
-      _______,  KC_4,     KC_5,     KC_6,     KC_EXLM,  KC_DLR,   KC_BSLS,  KC_MINS,  KC_PLUS,  KC_LCBR, KC_RCBR, KC_PIPE,
+      _______,  KC_4,     KC_5,     KC_6,     KC_EXLM,  KC_DLR,   KC_BSLS,  KC_MINS,  KC_PLUS,  KC_LCBR, KC_RCBR, KC_GRV,
       _______,  KC_1,     KC_2,     KC_3,     KC_AT,    KC_PERC,  KC_PIPE,  KC_UNDS,  KC_EQL,   KC_LBRC, KC_RBRC, XXXXXXX, 
       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MNXT,  KC_VOLD, KC_VOLU, KC_MPLY
   ),
